670e6ca2eec1d2ca6a3f06551c72b1b442de14d0
jimin-ni <jimin326ww@gmail.com>
2023-10-08 03:46:49 +0900

    나브 바 토글 설정 완료

--- js/home.js
+++ js/home.js
@@ -1,4 +1,4 @@
-//! 페이지가 스크롤될 때 발생하는 이벤트 리스너를 추가합니다.
+//! 나브 바 로고 - 스크롤하면 왼쪽으로 이동
 window.addEventListener("scroll", () => {
     const menuIcon = document.querySelector(".logoImg");
     const menu = document.querySelector(".menu");
@@ -10,16 +10,14 @@ window.addEventListener("scroll", () => {
     }
 });
 
-//! 스크롤 된 위치로 이동. 배포 후에는 이거 없이도 작동. 
-//* https://example.com/#section-to-scroll 이런식으로 주소를 달면 될 것. 
-document.querySelector('a[href="#scroll-to-section"]').addEventListener('click', function(e) {
-    e.preventDefault(); // 기본 링크 이벤트 제거
-    // 스크롤을 원하는 앵커 위치로 이동
-    const target = document.querySelector('#scroll-to-section');
-    window.scrollTo({
-        top: target.offsetTop,
-        behavior: 'smooth' // 부드러운 스크롤 애니메이션
+//! 나브 바 아이콘 숨기기 보이기
+$(document).ready(function() {
+    const navIcon = $("#nav-icon");
+    const navMenu = $("#nav-menu");
+
+    // 나브 아이콘을 클릭했을 때 이벤트 처리
+    navIcon.click(function() {
+        // 나브바 항목의 보이기/숨기기를 토글
+        navMenu.toggle();
     });
 });
-
-
