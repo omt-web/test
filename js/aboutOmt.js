ff9b1376cad530d8f2d960b3f5ea57f70053db37
jimin-ni <jimin326ww@gmail.com>
2023-10-12 03:06:22 +0900

    exhibition 페이지 아코디언 기능 가능

--- js/aboutOmt.js
+++ js/aboutOmt.js
@@ -1,15 +1,21 @@
-$(".show-more a").on("click", function() {
-    var $this = $(this);
-    var $content = $this.parent().prev("div.content");
-    var linkText = $this.text().toUpperCase();
-  
-    if (linkText === "SHOW MORE") {
-      linkText = "Show less";
-      $content.switchClass("hideContent", "showContent", 400);
-    } else {
-      linkText = "Show more";
-      $content.switchClass("showContent", "hideContent", 400);
-    };
-  
-    $this.text(linkText);
+//! 아코디언 
+document.addEventListener('DOMContentLoaded', function () {
+  var toggleButton = document.querySelector('.toggle-button');
+  var content = document.querySelector('.content');
+
+  // 초기 높이 설정
+  var initialHeight = 50; // CSS에서 설정한 max-height 값과 동일하게 설정합니다.
+  var isExpanded = false; // 내용이 현재 확장되었는지 나타내는 플래그
+
+  toggleButton.addEventListener('click', function () {
+      isExpanded = !isExpanded; // 상태 토글
+
+      if (isExpanded) {
+          // 내용을 확장
+          content.style.maxHeight = content.scrollHeight + "px";
+      } else {
+          // 내용을 접음
+          content.style.maxHeight = initialHeight + "px";
+      }
   });
+});
